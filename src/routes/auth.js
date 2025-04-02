f9568efd384f758057980f810e699e34bf5329f5
willenius <wilhelm.bjor@gmail.com>
2025-04-02 12:59:32 +0200

    pushing up register yet again to test if creating new users works as intended.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,7 +8,7 @@ import bcrypt from 'bcryptjs';
 const router = express.Router();
 
 // Register
-/*router.post('/register', async (req, res) => {
+router.post('/register', async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
@@ -17,12 +17,12 @@ const router = express.Router();
       { id: user._id, isAdmin: user.isAdmin },
       process.env.JWT_SECRET || 'your-secret-key'
     );
-
+    
     res.status(201).json({ user, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
-}); */
+});
 
 //TODO Login
 /*router.post('/login', async (req, res) => {
