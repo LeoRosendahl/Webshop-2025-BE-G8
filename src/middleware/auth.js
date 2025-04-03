28dc47fbadb59c82f62691852ba2da4c9ee83ea3
willenius <wilhelm.bjor@gmail.com>
2025-04-03 17:19:38 +0200

    pushar pga min egna idioti att jag satt på en annan dator i main-branschen... sorry björn o leo... x)

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,8 +5,21 @@ export const auth = async (req, res, next) => {
   return next();
 };
 
+//TODO Implement admin authentication
 export const adminAuth = async (req, res, next) => {
-  //TODO Implement admin authentication
-  return next();
+  try {
+    const token = req.header('authorization')?.split(` `)[1]
+
+    if(!token) return res.status(401).json({message: "You're not an admin user."});
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'your-secret-key');
+    if(!decoded.isAdmin) {
+      return res.status(403).json({message: "Only admins can perform this action"})
+    }
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({message: "Invalid token"});
+  };
 };
 
