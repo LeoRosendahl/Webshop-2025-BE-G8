996ea0662d0a3e59e266e50a1048c98fe7efe943
willenius <wilhelm.bjor@gmail.com>
2025-04-10 13:46:30 +0200

    DELETE funktion för användare

--- src/routes/minasidor.js
+++ src/routes/minasidor.js
@@ -5,15 +5,28 @@ import express from "express";
 const router = express.Router();
 
 //funktion för att kunna lägga till innehåll mina sidor
-router.get('/', auth, async(req, res) => {
+router.get('/', auth, async (req, res) => {
     try {
         const id = req.user.id
         const user = await User.findById(id).select("-password")
-        return res.json({user})
+        return res.json({ user })
 
     } catch (error) {
         console.error("Error adding user info");
         res.status(500).json({ error: "Internal server error" });
+    }
+})
+router.delete('/:id', auth, async (req, res) => {
+    try {
+
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.status(404).json({ error: "User not found" })
+        }
+        res.status(200).json({ message: `User with ID ${req.params.id} deleted successfully` });
+
+    } catch (error) {
+        res.status(500).json({ error: "Failed to delete user" })
     }
 })
 
@@ -27,11 +40,11 @@ router.put('/', auth, async (req, res) => {
         delete updateData.password
 
         const user = await User.findOneAndUpdate(
-          { _id: id },
-          {
-            $set: updateData
-          },
-          { new: true }
+            { _id: id },
+            {
+                $set: updateData
+            },
+            { new: true }
         ).select("-password");
 
         if (!user) {
@@ -40,7 +53,7 @@ router.put('/', auth, async (req, res) => {
 
         res.status(200).json({ message: "User info updated", user });
     } catch (error) {
-        console.error("Error updating user info" );
+        console.error("Error updating user info");
         res.status(500).json({ error: "Internal server error" });
     }
 });
