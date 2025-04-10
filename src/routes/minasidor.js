5e9def40bfaeea330fd003d0dc4dbd37b24d1bb4
willenius <wilhelm.bjor@gmail.com>
2025-04-10 13:56:15 +0200

    deleted auth from minasidor

--- src/routes/minasidor.js
+++ src/routes/minasidor.js
@@ -5,7 +5,7 @@ import express from "express";
 const router = express.Router();
 
 //funktion för att kunna lägga till innehåll mina sidor
-router.get('/', auth, async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const id = req.user.id
         const user = await User.findById(id).select("-password")
@@ -16,22 +16,20 @@ router.get('/', auth, async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
     }
 })
-router.delete('/:id', auth, async (req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
-
         const user = await User.findByIdAndDelete(req.params.id)
         if (!user) {
             return res.status(404).json({ error: "User not found" })
         }
         res.status(200).json({ message: `User with ID ${req.params.id} deleted successfully` });
-
     } catch (error) {
         res.status(500).json({ error: "Failed to delete user" })
     }
 })
 
 //funktion för att kunna ändra mina sidor.
-router.put('/', auth, async (req, res) => {
+router.put('/', async (req, res) => {
     try {
         const id = req.user.id
         //istället för att deconstructa allting igen
